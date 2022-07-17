ae6509ab90c61968972ec195d4815a3fe74ef24e
Christian Mattes <CarpeNecopinum@noreply.users.github.com>
2022-07-17 19:55:47 +0200

    Reconstruct the swifter-based basis

--- Package.swift
+++ Package.swift
@@ -7,14 +7,18 @@ let package = Package(
     name: "Swiftiger",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.5.0"))
+        .package(url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.5.0")),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.13.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Swiftiger",
-            dependencies: [.product(name: "Swifter", package: "swifter")]),
+            dependencies: [
+                .product(name: "Swifter", package: "swifter"),
+                .product(name: "SQLite", package: "SQLite.swift")                
+            ]),
         .testTarget(
             name: "SwiftigerTests",
             dependencies: ["Swiftiger"]),
