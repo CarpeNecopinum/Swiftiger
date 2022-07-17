9d942e2613d654a3003b802acc4d8f7598dc2b15
Christian Mattes <CarpeNecopinum@noreply.users.github.com>
2022-07-17 16:53:18 +0200

    Include swifter to make a Hello World web app

--- Sources/Swiftiger/main.swift
+++ Sources/Swiftiger/main.swift
@@ -1,1 +1,22 @@
-print("Hello, world!")
+import Swifter
+import Dispatch
+
+let server = HttpServer()
+
+server["/"] = scopes { 
+  html {
+    body {
+      h1 { inner = "Hello World!" }
+    }
+  }
+}
+
+let semaphore = DispatchSemaphore(value: 0)
+do {
+  try server.start(9080, forceIPv4: true)
+  print("Server has started ( port = \(try server.port()) ). Try to connect now...")
+  semaphore.wait()
+} catch {
+  print("Server start error: \(error)")
+  semaphore.signal()
+}
